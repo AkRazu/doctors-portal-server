c015e27cc785516715327a7d0d6e6ea4fb7185d8
Ak Razu <fbook3603@gmail.com>
2023-03-06 23:31:19 +0600

    add appointment specialty on db

--- index.js
+++ index.js
@@ -65,6 +65,15 @@ async function run() {
       res.send(options);
     });
 
+    app.get("/appointmentSpecialty", async (req, res) => {
+      const query = {};
+      const result = await appointmentOptionCollection
+        .find(query)
+        .project({ name: 1 })
+        .toArray();
+      res.send(result);
+    });
+
     /**** 
             name always be the same
         */
