c0bb8662262ccc776776193300013674fec2827d
Ak Razu <fbook3603@gmail.com>
2023-03-09 21:58:52 +0600

    booking params db add

--- index.js
+++ index.js
@@ -38,7 +38,9 @@ async function run() {
     const appointmentOptionCollection = client
       .db("doctorsPortal")
       .collection("appointmentOptions");
-    const bookingCollection = client.db("doctorsPortal").collection("bookings");
+    const bookingsCollection = client
+      .db("doctorsPortal")
+      .collection("bookings");
     const userCollection = client.db("doctorsPortal").collection("users");
     const doctorsCollection = client.db("doctorsPortal").collection("doctors");
 
@@ -54,16 +56,19 @@ async function run() {
       next();
     };
 
+    // Use Aggregate to query multiple collection and then merge data
     app.get("/appointmentOptions", async (req, res) => {
       const date = req.query.date;
-      console.log(date);
       const query = {};
       const options = await appointmentOptionCollection.find(query).toArray();
+
+      // get the bookings of the provided date
       const bookingQuery = { appointmentDate: date };
-      const alreadyBooked = await bookingCollection
+      const alreadyBooked = await bookingsCollection
         .find(bookingQuery)
         .toArray();
 
+      // code carefully :D
       options.forEach((option) => {
         const optionBooked = alreadyBooked.filter(
           (book) => book.treatment === option.name
@@ -73,33 +78,111 @@ async function run() {
           (slot) => !bookedSlots.includes(slot)
         );
         option.slots = remainingSlots;
-        console.log(date, option.name, remainingSlots.length);
       });
       res.send(options);
     });
 
-    app.get("/appointmentSpecialty", async (req, res) => {
+    app.get("/appointmentOptions", async (req, res) => {
+      const date = req.query.date;
       const query = {};
-      const result = await appointmentOptionCollection
-        .find(query)
-        .project({ name: 1 })
+      const options = await appointmentOptionCollection.find(query).toArray();
+
+      // get the bookings of the provided date
+      const bookingQuery = { appointmentDate: date };
+      const alreadyBooked = await bookingsCollection
+        .find(bookingQuery)
         .toArray();
-      res.send(result);
+
+      // code carefully :D
+      options.forEach((option) => {
+        const optionBooked = alreadyBooked.filter(
+          (book) => book.treatment === option.name
+        );
+        const bookedSlots = optionBooked.map((book) => book.slot);
+        const remainingSlots = option.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        option.slots = remainingSlots;
+      });
+      res.send(options);
     });
 
-    /**** 
-            name always be the same
-        */
+    app.get("/v2/appointmentOptions", async (req, res) => {
+      const date = req.query.date;
+      const options = await appointmentOptionCollection
+        .aggregate([
+          {
+            $lookup: {
+              from: "bookings",
+              localField: "name",
+              foreignField: "treatment",
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $eq: ["$appointmentDate", date],
+                    },
+                  },
+                },
+              ],
+              as: "booked",
+            },
+          },
+          {
+            $project: {
+              name: 1,
+              price: 1,
+              slots: 1,
+              booked: {
+                $map: {
+                  input: "$booked",
+                  as: "book",
+                  in: "$$book.slot",
+                },
+              },
+            },
+          },
+          {
+            $project: {
+              name: 1,
+              price: 1,
+              slots: {
+                $setDifference: ["$slots", "$booked"],
+              },
+            },
+          },
+        ])
+        .toArray();
+      res.send(options);
+    });
+
+    /***
+     * API Naming Convention
+     * app.get('/bookings')
+     * app.get('/bookings/:id')
+     * app.post('/bookings')
+     * app.patch('/bookings/:id')
+     * app.delete('/bookings/:id')
+     */
 
     app.get("/bookings", verifyJWT, async (req, res) => {
       const email = req.query.email;
-      const decodedEmail = req.query.email;
+      const decodedEmail = req.decoded.email;
+
       if (email !== decodedEmail) {
         return res.status(403).send({ message: "forbidden access" });
       }
+
       const query = { email: email };
-      const booking = await bookingCollection.find(query).toArray();
-      res.send(booking);
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
+    });
+
+    app.get("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingsCollection.findOne(query);
+      res.send(result);
     });
 
     app.post("/bookings", async (req, res) => {
@@ -107,19 +190,20 @@ async function run() {
       console.log(booking);
       const query = {
         appointmentDate: booking.appointmentDate,
+        email: booking.email,
         treatment: booking.treatment,
       };
-      const alreadyBooked = await bookingCollection.find(query).toArray();
+
+      const alreadyBooked = await bookingsCollection.find(query).toArray();
 
       if (alreadyBooked.length) {
-        const message = `You already have a booking ${booking.appointmentDate}`;
+        const message = `You already have a booking on ${booking.appointmentDate}`;
         return res.send({ acknowledged: false, message });
       }
 
-      const result = await bookingCollection.insertOne(booking);
+      const result = await bookingsCollection.insertOne(booking);
       res.send(result);
     });
-
     app.get("/jwt", async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
@@ -184,7 +268,6 @@ async function run() {
       );
       res.send(result);
     }); */
-    
 
     app.get("/doctors", verifyJWT, verifyAdmin, async (req, res) => {
       const query = {};
